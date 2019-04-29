33aceb26ebf1261640596620d367dbc0851e7af9
Xiaoyu Hu <hufk@bc.edu>
2019-04-29 11:07:26 -0400

    Initial Commit

---
+++ 2SeeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2SeeApp
+//
+//  Created by Xiaoyu Hu on 4/29/19.
+//  Copyright © 2019 Xiaoyu Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
